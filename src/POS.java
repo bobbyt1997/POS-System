47e58dbd684be162e24044ce2fcf49f7efb2ae51
Bobby Ton <bobbyt1997@gmail.com>
2017-03-23 16:09:23 -0500

    Created a constructor and created JFrame window

--- src/POS.java
+++ src/POS.java
@@ -1,7 +1,24 @@
 
-public class POS {
+import javax.swing.JFrame;
+
+public class POS extends JFrame{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public static void main(String[] args){
-		
+		new POS();
 	}
+
+	public POS(){
+		//create the window using JFrame
+        super("Nail Tech POS System");
+        setVisible(true);
+        setSize(1000, 800);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
 }
