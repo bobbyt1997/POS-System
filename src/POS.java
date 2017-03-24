0a7cdd5dfd812bccbce00590af6378e224a995d6
Bobby Ton <bobbyt1997@gmail.com>
2017-03-24 00:03:44 -0500

    Set up main window for the program. Use buttons to switch between four different windows

--- src/POS.java
+++ src/POS.java
@@ -1,5 +1,15 @@
 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public class POS extends JFrame{
 	
@@ -9,16 +19,98 @@ public class POS extends JFrame{
 	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args){
-		new POS();
+		SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new POS();
+            }
+        });
 	}
 
 	public POS(){
-		//create the window using JFrame
-        super("Nail Tech POS System");
-        setVisible(true);
-        setSize(1000, 800);
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Create window using jframe
+		JFrame frame = new JFrame("Nail Tech POS System");
+	
+		frame.setVisible(true);
+		frame.setSize(1000, 800);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+		CardLayout cl = new CardLayout();
+		
+		//Create all necessary jpanels
+		JPanel jframe = new JPanel(new BorderLayout());
+        JPanel tabs = new JPanel(cl);
+        JPanel buttons = new JPanel();
+        JPanel settingsTab = new JPanel();
+        JPanel curTransTab = new JPanel();
+        JPanel cloTransTab = new JPanel();
+        JPanel dailyTab = new JPanel();
+        
+        //CHANGE COLOR OF BACKGROUNDS FOR TESTING
+        settingsTab.setBackground(Color.BLUE);
+        curTransTab.setBackground(Color.RED);
+        cloTransTab.setBackground(Color.GRAY);
+        dailyTab.setBackground(Color.BLACK);
+        
+        //Create buttons
+        JButton settingsBtn = new JButton("Settings");
+        JButton currentTransactionsBtn = new JButton("Current Transaction");
+        JButton closedTransactionsBtn = new JButton("Closed Transactions");
+        JButton dailyReportBtn = new JButton("Settings");
+        
+        //Add jpanels to the tabs jpanel, which is using cardLayout
+        tabs.add(settingsTab, "Settings");
+        tabs.add(curTransTab, "Current Transactions");
+        tabs.add(cloTransTab, "Closed Transactions");
+        tabs.add(dailyTab, "Daily Reports");
+        
+        //Add existing buttons to the buttons jpanel
+        buttons.add(settingsBtn);
+        buttons.add(currentTransactionsBtn);
+        buttons.add(closedTransactionsBtn);
+        buttons.add(dailyReportBtn);
+        
+        //Set up buttons to switch jpanels based on which button was clicked
+        settingsBtn.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cl.show(tabs, "Settings");
+				
+			}
+        });
+        
+        currentTransactionsBtn.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cl.show(tabs, "Current Transactions");
+				
+			}
+        });
+        
+        closedTransactionsBtn.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cl.show(tabs, "Closed Transactions");
+				
+			}
+        });
+        
+        dailyReportBtn.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cl.show(tabs, "Daily Reports");
+				
+			}
+        });
+        
+        //Add the tabs and buttons jpanel to the main jframe panel
+        jframe.add(tabs, BorderLayout.CENTER);
+        jframe.add(buttons, BorderLayout.PAGE_START);
+        
+        //Add the main panel (jframe) to JFrame
+        frame.add(jframe);
+        
 	}
 	
 }
