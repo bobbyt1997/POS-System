ce7ab7d4d8791203ce903588edc780bdf3c43084
Bobby Ton <bobbyt1997@gmail.com>
2017-09-02 12:10:06 -0500

    Created user class to create or remove users.

---
+++ src/Customer.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class Customer {
+	//JDBC driver name and database URL
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost:3306/pos?useSSL=false";
+						  
+	//Database credentials
+	static final String USER = "root";
+	static final String PASS = "42597";
+	static Connection conn = null;
+	static Statement stmt = null;
+	
+	String lastName;
+	String firstName;
+	
+	public Customer(String pLastName, String pFirstName){
+		lastName = pLastName;
+		firstName = pFirstName;
+	}
+	
+	public String getLastName(){
+		return lastName;
+	}
+	
+	public String getFirstName(){
+		return firstName;
+	}
+}
