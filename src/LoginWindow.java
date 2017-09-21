cfda3eaae7d48f9ae2deb77241c5d76467c03c5b
Bobby Ton <bobbyt1997@gmail.com>
2017-09-20 19:37:50 -0500

    Added functionality to the login button. If clicked, the button will check the credentials against the hardcoded credentials. If all information matches, the user will be given access to the program.

--- src/LoginWindow.java
+++ src/LoginWindow.java
@@ -2,6 +2,9 @@
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 import javax.swing.*;
 
@@ -15,12 +18,12 @@ public class LoginWindow extends JFrame{
 		//create elements that will go into the login window
 		JButton loginBtn = new JButton("Log In");
 		JTextField usernameField = new JTextField(25);
-		JTextField passwordField = new JTextField(25);
+		JPasswordField passwordField = new JPasswordField(25);
 		JLabel usernameLbl = new JLabel("Username: ");
 		JLabel passwordLbl = new JLabel("Password: ");
 		
 		frame.setVisible(true);
-		frame.setSize(500, 200);
+		frame.setSize(500, 300);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(null);
@@ -33,12 +36,56 @@ public class LoginWindow extends JFrame{
 		frame.add(loginBtn);
 		
 		//position and size each element
-		usernameLbl.setBounds(140, 20, 80, 30);
-		usernameField.setBounds(210, 20, 150, 30);
-		passwordLbl.setBounds(140, 60, 80, 30);
-		passwordField.setBounds(210, 60, 150, 30);
-		loginBtn.setBounds(210, 100, 100, 30);
+		usernameLbl.setBounds(140, 70, 80, 30);
+		usernameField.setBounds(210, 70, 150, 30);
+		passwordLbl.setBounds(140, 110, 80, 30);
+		passwordField.setBounds(210, 110, 150, 30);
+		loginBtn.setBounds(210, 150, 100, 30);
 		
+		//create a listener that does something when the button is clicked
+		loginBtn.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//check if credentials entered matches any data in the database
+				if ( checkCredentials(usernameField.getText(), passwordField.getPassword()) == true ){
+					SwingUtilities.invokeLater(new Runnable() {
+			            @Override
+			            public void run() {
+			            	//Start a new instance of POS and dispose of the login window
+			                new POS();
+			                dispose();
+			            }
+			        });
+				}
+				else{
+					//if either usename or password is incorrect, notify the user
+					JOptionPane.showMessageDialog(frame, "Username or Password INCORRECT. Please try again.");
+				}
+			}
+        });
+		
+		
+	}
+	
+	/**
+	 * 
+	 * @param username username entered by the user
+	 * @param password password entered by the user retrieved as an array of chars
+	 * @return true if credentials matches the data in the database and false otherwise
+	 */
+	public static boolean checkCredentials(String username, char[] password){
+		String correctPass = "dude";
+		String correctUser = "bobby";
+		
+		if (username.equals(correctUser)){
+			if (Arrays.equals(password, correctPass.toCharArray())){
+				return true;
+			}
+			else
+				return false;
+		}
+		else
+			return false;
 	}
 	
 	public static void main(String[] args){
