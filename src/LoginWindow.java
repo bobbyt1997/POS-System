5fdd7dfb079b1f8e0b84956fefa7132dd6c023d6
Bobby Ton <bobbyt1997@gmail.com>
2017-09-20 19:09:58 -0500

    Designed login window: functionality not finished

--- src/LoginWindow.java
+++ src/LoginWindow.java
@@ -1,5 +1,9 @@
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.*;
 
 public class LoginWindow extends JFrame{
 
@@ -8,16 +12,33 @@ public class LoginWindow extends JFrame{
 	public LoginWindow(){
 		JFrame frame = new JFrame("Nail Tech POS System");
 		
+		//create elements that will go into the login window
+		JButton loginBtn = new JButton("Log In");
+		JTextField usernameField = new JTextField(25);
+		JTextField passwordField = new JTextField(25);
+		JLabel usernameLbl = new JLabel("Username: ");
+		JLabel passwordLbl = new JLabel("Password: ");
+		
 		frame.setVisible(true);
-		frame.setSize(1000, 800);
+		frame.setSize(500, 200);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//set layout for main frame
+		frame.setLayout(null);
 		
 		//Create a username and password textbox with a login button at the bottom
-				
-		//If user name and password matches one from a table in mysql, display a list of customer to choose from.
+		frame.add(usernameLbl);
+		frame.add(usernameField);
+		frame.add(passwordLbl);
+		frame.add(passwordField);
+		frame.add(loginBtn);
+		
+		//position and size each element
+		usernameLbl.setBounds(140, 20, 80, 30);
+		usernameField.setBounds(210, 20, 150, 30);
+		passwordLbl.setBounds(140, 60, 80, 30);
+		passwordField.setBounds(210, 60, 150, 30);
+		loginBtn.setBounds(210, 100, 100, 30);
+		
 	}
 	
 	public static void main(String[] args){
